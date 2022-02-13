f2538d930ee728f047f00a9bafa41aa7c81ecd48
Przemyslaw Pirog <peterpirogtf@gmail.com>
2022-02-13 16:38:06 +0100

    initial commit

--- helper_functions.py
+++ helper_functions.py
@@ -1,5 +1,7 @@
 import numpy as np
+import os
 import pandas as pd
+pd.pandas.set_option('display.max_rows', None,'display.max_columns', None)
 
 def test_function():
     print('Test function correctly imported')
@@ -7,7 +9,11 @@ def test_function():
 
 
 
-def dataframe_analysis(df: pd.dataframe) -> pd.dataframe:
+def dataframe_analysis(df,xls_filename='Columns_analysis.xlsx'):
+    # Delete old analysis file if exist
+    if os.path.exists(xls_filename):
+        os.remove(xls_filename)
+
     # Analysis of  unique values
     output = []
 
@@ -21,5 +27,22 @@ def dataframe_analysis(df: pd.dataframe) -> pd.dataframe:
     output = pd.DataFrame(output)
     output.columns = ['colName', 'non-null values', 'unique', 'dtype']
     output = output.sort_values(by='unique', ascending=False)
-    output.to_excel("Columns_analysis.xlsx", sheet_name='Columns')
-    return output
+    output.to_excel(xls_filename, sheet_name='Columns')
+
+    # Return categorical columns
+    # get all categorical columns in the dataframe
+    catCols = [col for col in df.columns if df[col].dtype == "O"]
+    numCols = [col for col in df.columns if not df[col].dtype == "O"]
+    print(output)
+    return output, catCols, numCols
+
+if __name__ == '__main__':
+
+
+    FILE_PATH = 'dataset.csv'
+    DIR_DATA = '/content/drive/MyDrive/datasets/cars/'
+
+    # load dataset
+    df = pd.read_csv(FILE_PATH, delimiter=';')
+    #print(df.head())
+    dataframe_analysis(df)
